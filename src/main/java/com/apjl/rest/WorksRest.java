69fa8c63f84b97fca087b05a846c00cfc1ebcf8f
Javier Garcia Montañés <javier.garcia-montanes.external@capgemini.com>
2023-06-29 14:47:31 +0200

    ultimos cambios

--- src/main/java/com/apjl/rest/WorksRest.java
+++ src/main/java/com/apjl/rest/WorksRest.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apjl.dao.WorksDAO;
+import com.apjl.model.Customers;
 import com.apjl.model.Works;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -33,8 +35,30 @@ public class WorksRest {
 		return workDAO.findAll();
 	}
 	
+	@GetMapping("/listar/{id}")
+	public Works getWorkById(@PathVariable("id") Integer id){
+		return workDAO.findById(id).get();
+	}
+	
 	@DeleteMapping("/eliminar/{id}")
 	public void deleteWork(@PathVariable("id") Integer id) {
 		workDAO.deleteById(id);
+	}
+	
+	@PutMapping("/actualizar/{id}")
+	public void updateWork(@PathVariable("id") Integer id, @RequestBody Works work) {
+		
+		Works workAux = workDAO.findById(id).get();
+		
+		workAux.setName(work.getName());
+		workAux.setDescription(work.getDescription());
+		workAux.setAddress(work.getAddress());
+		workAux.setCity(work.getCity());
+		workAux.setPrice(work.getPrice());
+		workAux.setPaid(work.getPaid());
+		workAux.setCustomer(work.getCustomer());
+		
+		workDAO.save(workAux);
+		
 	}
 }
